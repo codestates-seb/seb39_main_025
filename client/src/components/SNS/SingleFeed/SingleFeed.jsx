a8c68ed28ae4ec5ecd6e237873d358b6d18c0a52
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-10-02 23:09:22 +0900

    fix: 엔드포인트 요청 http -> https 변경

--- client/src/components/SNS/SingleFeed/SingleFeed.jsx
+++ client/src/components/SNS/SingleFeed/SingleFeed.jsx
@@ -32,7 +32,7 @@ function SingleFeed() {
   useEffect(() => {
     const getFeed = async () => {
       const res = await axios.get(
-        `http://www.server.staybuddy.net/api/posts`,
+        `https://server.staybuddy.net/api/posts`,
         myConfig,
       );
       const data = await res.data.data;
