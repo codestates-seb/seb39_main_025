b5efebe7b8243de157d1a4ee55f78e10737e796d
songseul <song.codestates@gmail.com>
2022-10-06 16:10:47 +0900

    fix:백엔드 게시물 테스트

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -5,7 +5,7 @@ import Spinner from '../../assets/loading.gif';
 function Loading() {
   return (
     <Background>
-      <img src={Spinner} alt="로딩중" width="30%" />
+      <img src={Spinner} alt="로딩중" width="10%" />
       <LoadingText> 잠시만 기다려 주세요...</LoadingText>
     </Background>
   );
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,27 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
-import SingleFeed from '../components/SNS/SingleFeed/SingleFeed';
 import Stories from '../components/SNS/Story/Stories';
-import SuggestInfinite from '../components/SNS/Suggestion/SuggestInfinite';
 
 function HomePage() {
-  return (
-    <>
-      <Stories />
-      <FeedBox>
-        <SingleFeed />
-        {/* <SuggestInfinite /> */}
-      </FeedBox>
-    </>
-  );
+  return <Stories />;
 }
 
 export default HomePage;
-
-const FeedBox = styled.div`
-  display: flex;
-
-  @media screen and (max-width: 1200px) {
-    flex-direction: column-reverse;
-  }
-`;
